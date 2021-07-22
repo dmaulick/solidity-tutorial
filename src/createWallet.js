c5088cf96c0615ba08b7bd69ed38fe2993cdcb4b
David Maulick <david.maulick@coinbase.com>
2021-07-22 13:17:49 -0700

    get USDC

--- src/createWallet.js
+++ src/createWallet.js
@@ -9,4 +9,5 @@ console.log(`Address: ${wallet.address}`);
   Test Wallet on Ropsten:
   Mnemonic: decrease gentle derive place negative author crawl enemy unlock blouse twin genius
   Address: 0x017A0b0e99F3b5cEAD1C3C37c48bD2F16547baCF
+  MetaMask pw: TestRopsten
 */
--- src/getBalance.js
+++ src/getBalance.js
@@ -2,11 +2,24 @@ const ethers = require("ethers");
 const wallet = require("./wallet");
 const provider = require("./provider");
 
-// Programmatically get balance
 async function main() {
   const account = wallet.connect(provider);
-  const balance = await account.getBalance();
-  console.log(`ETH Balance: ${ethers.utils.formatUnits(balance, 18)}`);
+
+  // Define contract interface
+  const usdc = new ethers.Contract(
+    "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
+    [
+      "function balanceOf(address _owner) public view returns (uint256 balance)",
+    ],
+    account
+  );
+
+  const ethBalance = await account.getBalance();
+  console.log(`ETH Balance: ${ethers.utils.formatEther(ethBalance)}`);
+
+  // Call balanceOf function
+  const usdcBalance = await usdc.balanceOf(account.address);
+  console.log(`USDC Balance: ${ethers.utils.formatUnits(usdcBalance, 6)}`);
 }
 
 main();
